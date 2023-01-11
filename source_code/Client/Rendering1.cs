1789fe6ba7ffa86ad0f08f8d47d7f3d371b0636b
yumekaC <64627840+yumekaC@users.noreply.github.com>
2023-01-11 18:16:45 +0900

    Create Rendering1.cs

---
+++ source_code/Client/Rendering1.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Net;
+
+public class Rendering1 : MonoBehaviour
+{
+    public DateTime download1;
+    public DateTime download2;
+    public DateTime display_time2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        byte[] wwwdata = null;
+        string url = "http://XXXX/ply_dataset/x.ply";//binary ply
+        while (true)
+        {
+            try
+            {
+                //DateTime dl_ts = DateTime.UtcNow;
+                //dlts = dl_ts.Year + "-" + dl_ts.Month + "-" + dl_ts.Day + "T" + dl_ts.Hour + ":" + dl_ts.Minute + ":" + dl_ts.Second + "." + dl_ts.Millisecond + "Z";
+                download1 = DateTime.Now;
+                WebRequest req = WebRequest.Create(url);
+                using (WebResponse res = req.GetResponse())
+                {
+                    using (Stream st = res.GetResponseStream())
+                    {
+                        wwwdata = MyClass.ReadBinaryData(st);
+                        if (wwwdata != null)
+                        {
+                            download2 = DateTime.Now;
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                if (ex.Status == WebExceptionStatus.ProtocolError)
+                {
+                    //Debug.Log(ex.Message);
+                }
+                else
+                {
+                    //Debug.Log(ex.Message);
+                }
+            }
+        }
+
+        Stream stream = new MemoryStream(wwwdata);
+        var gameObject = new GameObject();
+        gameObject.name = "frame_chair";
+
+        var mesh = MyClass.ImportAsMesh(stream);
+        var meshFilter = gameObject.AddComponent<MeshFilter>();
+        meshFilter.sharedMesh = mesh;
+        var meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        meshRenderer.sharedMaterial = MyClass.GetDefaultMaterial();
+
+        gameObject.transform.position = new Vector3(0, 0, 0.5f);
+        gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+        gameObject.transform.localScale = new Vector3(1, 1, 1);
+        display_time2 = DateTime.Now;
+        
+        TimeSpan download_ts = download2 - download1;
+        Debug.Log(download_ts + ":download_ts");
+        TimeSpan display_ts = display_time2 - download2;
+        Debug.Log(display_ts + ":display_ts");
+    }
+}
